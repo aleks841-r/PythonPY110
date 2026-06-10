483fd8e3ddb6044b9b29d25460824656697760d0
Gosha <gosha-s_email@yandex.ru>
2026-06-10 22:04:38 +0300

    Обновлен, lab6_exam

--- app_wishlist/urls.py
+++ app_wishlist/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import wishlist_view
-#  TODO Импортируйте ваше представление
+from .views import (
+    wishlist_view,
+    wishlist_add_view,
+    wishlist_remove_view,
+    wishlist_view_json,
+    wishlist_add_view_json,
+    wishlist_del_view_json,
+)
 
 app_name = 'app_wishlist'
 
 urlpatterns = [
-    path('', wishlist_view, name='wishlist_view'),  # TODO Зарегистрируйте обработчик
+    path('', wishlist_view, name='wishlist_view'),
+    path('add/<str:id_product>', wishlist_add_view, name='wishlist_add'),
+    path('remove/<str:id_product>', wishlist_remove_view, name="wishlist_remove"),
+    path('api/', wishlist_view_json),
+    path('api/add/<id_product>', wishlist_add_view_json),
+    path('api/del/<id_product>', wishlist_del_view_json),
 ]
--- app_wishlist/views.py
+++ app_wishlist/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth import get_user
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, HttpResponseNotFound
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 
 from app_store.models import DATABASE
 from logic.control_wishlist import view_in_wishlist, add_to_wishlist, remove_from_wishlist
@@ -10,9 +9,12 @@ from logic.control_wishlist import view_in_wishlist, add_to_wishlist, remove_fro
 
 @login_required(login_url='app_login:login_view')
 def wishlist_view(request):
+    """
+    Просмотр всех продуктов в избранном у пользователя
+    """
     if request.method == "GET":
         username = get_user(request).username
-        data = view_in_wishlist(username)[username]  # Получаем избранное для пользователя username
+        data = view_in_wishlist(username)[username] # Получаем избранное для пользователя username
 
         products = []  # Список продуктов
         for product_id in data['products']:
@@ -21,15 +23,74 @@ def wishlist_view(request):
                 product_info['id'] = product_id # Добавляем идентификатор продукта
                 products.append(product_info) # Создаем список словарей избранных продуктов
 
+        # Передать HTML-страницу с сервере
         return render(request, "app_wishlist/wishlist.html", context={"products": products})
 
 
 @login_required(login_url='app_login:login_view')
 def wishlist_add_view(request, id_product):
+    """
+    Добавление нового продукта в избранное у пользователя
+    """
+    if request.method == "GET":
+        username = get_user(request).username
+        result = add_to_wishlist(id_product, username) # Добавляем новый продукт в избранное
+        if result:
+            # Перенаправляем на именованный маршрут списка избранного
+            return redirect("app_wishlist:wishlist_view")
+
+    return HttpResponseNotFound("Неудачное добавление в избранное")
+
+
+@login_required(login_url='app_login:login_view')
+def wishlist_remove_view(request, id_product):
+    """
+    Удаление продукта из избранного у пользователя
+    """
+    if request.method == "GET":
+        username = get_user(request).username
+        result = remove_from_wishlist(id_product, username) # Удаляем продукт из избранного
+        if result:
+            # Перенаправляем на именованный маршрут списка избранного
+            return redirect("app_wishlist:wishlist_view")
+
+    return HttpResponseNotFound("Неудачное удаление из избранного")
+
+
+@login_required(login_url='app_login:login_view')
+def wishlist_view_json(request):
+    """
+    Просмотр всех продуктов в избранном для пользователя и возвращение этого в JSON
+    """
+    if request.method == "GET":
+        username = get_user(request).username
+        data = view_in_wishlist(username)[username] # Получаем избранное для пользователя username
+        return JsonResponse(data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
+
+
+@login_required(login_url='app_login:login_view')
+def wishlist_add_view_json(request, id_product: str):
+    """
+    Добавление продукта в избранное и возвращение информации об успехе или неудаче в JSON
+    """
     if request.method == "GET":
         username = get_user(request).username
         result = add_to_wishlist(id_product, username)
         if result:
-            return redirect("app_wishlist:wishlist_view")
+            return JsonResponse({"answer": "Продукт успешно добавлен в избранное"})
 
-    return HttpResponseNotFound("Неудачное добавление в корзину")
+        return JsonResponse({"answer": "Неудачное добавление в избранное"}, status=404)
+
+
+@login_required(login_url='app_login:login_view')
+def wishlist_del_view_json(request, id_product: str):
+    """
+    Удаление продукта из избранного и возвращение информации об успехе или неудаче в JSON
+    """
+    if request.method == "GET":
+        username = get_user(request).username
+        result = remove_from_wishlist(id_product, username)
+        if result:
+            return JsonResponse({"answer": "Продукт успешно удалён из избранного"})
+
+        return JsonResponse({"answer": "Неудачное удаление из избранного"}, status=404)
